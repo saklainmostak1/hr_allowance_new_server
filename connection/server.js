4d46083aa441083fed3d01e4725c542482cf3002
saklainmostak1 <saklain15-12003@diu.edu.bd>
2024-09-25 10:32:07 +0600

    push

--- connection/server.js
+++ connection/server.js
@@ -2260,10 +2260,10 @@ app.get('/', (req, res) => {
 
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
 
 
